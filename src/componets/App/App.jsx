34c98ebcf0832291b7fa49317aa6b0c51464b693
$chouty <aschou101229@gmail.com>
2025-04-07 08:48:48 -0500

    Updated UI and added data

--- src/componets/App/App.jsx
+++ src/componets/App/App.jsx
@@ -3,17 +3,25 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import Modal from "../Modal/Modal";
+import AppContext from "../../context/AppContext";
 
 import "./App.css";
 
 function App() {
+  const [modal, setModal] = useState(null);
+
+
   return (
-    <>
+    <AppContext.Provider 
+    value={{
+      modal,
+      setModal
+    }}>
       <Header></Header>
       <Main></Main>
       <Footer></Footer>
       <Modal></Modal>
-    </>
+    </AppContext.Provider>
   );
 }
 
--- src/componets/Card/Card.jsx
+++ src/componets/Card/Card.jsx
@@ -2,11 +2,20 @@ import "./Card.css";
 function Card(card) {
   console.log(card.card.title);
   return (
-    <div className="card">
-      <img src={card.card.url} alt={card.card.title} className="card__img" />
-      <h3 className="card__title">{card.card.title}</h3>
-      <p className="card__description">{card.card.description}</p>
-    </div>
+    <a
+      href={card.card.url_repo}
+      className="card__link"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="card">
+        <img src={card.card.url} alt={card.card.title} className="card__img" />
+        <div className="card__content">
+          <h3 className="card__title">{card.card.title}</h3>
+          <p className="card__description">{card.card.description}</p>
+        </div>
+      </div>
+    </a>
   );
 }
 
